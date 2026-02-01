42cfab9645a20f9c7bf15c3230e93ca341340635
Prathamesh Doni <creatives.doni@gmail.com>
2026-02-01 12:28:49 +0530

    WorkSpace2 Added

--- CPP/WorkSpace1.cpp
+++ CPP/WorkSpace1.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-namespace A{
-    int A=100;
+namespace A
+{
+    int A = 100;
 };
 
-int main(){
-    cout<<"Hello, World!"<<endl;
-    int A=100;
+int main()
+{
+    cout << "Hello, World!" << endl;
+    int A = 100;
 
-    cout<<A<<endl;        // Local variable A
-    cout<<A::A<<endl;  // Namespace A's variable A
-
-
+    cout << A << endl;    // Local variable A
+    cout << A::A << endl; // Namespace A's variable A
 
     return 0;
 }
---
+++ CPP/WorkSpace2.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "WorkSpace2.cpp" << endl;
+    return 0;
+}
